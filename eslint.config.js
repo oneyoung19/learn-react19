1e3edc52cc6667b6c5e6549fdd139d6dd83a996d
Andy Yang <2878847069@qq.com>
2025-12-04 11:28:29 +0800

    build(config): add prettier and eslint configurations for code formatting and linting
    - Add .prettierrc.js and .prettierignore to configure Prettier formatting rules
    - Add eslint.config.js to set up ESLint with TypeScript, React, and Prettier integration
    - Update package.json scripts to include lint and format commands
    - Add necessary devDependencies for ESLint and Prettier tooling

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescript,
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/prop-types": "off",
+      "no-undef": "off",
+      "no-empty-pattern": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  prettier,
+  {
+    ignores: [
+      "**/build/**",
+      "**/.react-router/**",
+      "**/node_modules/**",
+      "**/dist/**",
+    ],
+  },
+];
+
